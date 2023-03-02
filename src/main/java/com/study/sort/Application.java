b9e03446febd339a73a7daaa65268a5324aed77a
huangqijun <Admin_117>
2023-03-02 20:52:43 +0800

    create project

---
+++ src/main/java/com/study/sort/Application.java
@@ -0,0 +1,13 @@
+package com.study.sort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
